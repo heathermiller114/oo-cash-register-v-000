af5191f8a356c8ffa5d966bb14e84dac49e34647
Heather Miller <heather.miller114@gmail.com>
2018-09-21 01:22:21 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,11 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
     @total += price * quantity
-    @@items << title
+    counter = 1
+    while counter < quantity
+      @@items << title
+      counter += 1
+    end
   end
   
   def apply_discount
